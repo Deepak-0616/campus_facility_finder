aef910532632f0138f3f97ea181e7d752cc8fc0d
Deepak-0616 <deepaksabari0616@gmail.com>
2025-09-01 22:27:23 +0530

    new

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.top = e.clientY + "px";  // use clientY
+  cursor.style.left = e.clientX + "px"; // use clientX
+});
+
+// Hover effect for links and buttons
+const hoverables = document.querySelectorAll("a, button");
+
+hoverables.forEach(el => {
+  el.addEventListener("mouseenter", () => {
+    cursor.style.transform = "translate(-50%, -50%) scale(1.8)";
+  });
+  el.addEventListener("mouseleave", () => {
+    cursor.style.transform = "translate(-50%, -50%) scale(1)";
+  });
+});
+
